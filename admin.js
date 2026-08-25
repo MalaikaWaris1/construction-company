0fec352e95145a4a97b5ee9c437fab0ef3d38ae8
Malaika Waris <MalaikaWaris1@gmail.com>
2026-08-25 13:18:27 +0500

    niii

--- admin.js
+++ admin.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     
     // --- REAL BACKEND API CONFIGURATION ---
-   const API_BASE_URL = window.location.hostname === "localhost" 
-  ? "http://localhost:8000" 
-  : "https://backend-construction-94n3.onrender.com";
+  const API_BASE_URL = window.location.hostname === "localhost" 
+  ? "http://localhost:8000/api" 
+  : "https://backend-construction-94n3.onrender.com/api";
     
     // Arrays backend se live sync honge
     let projectsArray = [];
